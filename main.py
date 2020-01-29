8f59a96f438060e712aabfc1a27c4524b1a6aa9d
eqvt <eqvtdev@gmail.com>
2020-01-29 15:15:41 +0100

    Unzips all files and moves to zip folder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # import modules
-import os
+import zipfile
+import pathlib
+import shutil
 
 # project info
 __version__ = "0.1.0"
@@ -7,15 +9,22 @@ __version__ = "0.1.0"
 # main program
 
 def main():    
-    print(f"Your current directory is: {dir_start}")
-    action = input("Write 'Start' to proceed: ")
-    if action == "Start"S:
-        print("Sucessful")
-    else:
-        print("Quiting...")
-        exit()
-
-if __name__ == "__main__":
-    # This is executed from the command line 
-    dir_start = os.path.dirname(os.path.realpath(__file__))
+    print(f"Your current directory is: {dir_start}")    
+    for path in dir_start.rglob('*.zip'):
+        if path.parent != dir_zip:            
+            try:
+                zf = zipfile.ZipFile(path)
+                zf.extractall(path.parent)
+                zf.close()            
+                source = str(path)  
+                print(source)          
+                destination = str(dir_zip.joinpath(path.name))
+                print(destination)
+                shutil.move(source, destination)                
+            except:
+                print(f"Failed to unpack the archive: {path.name}")
+  
+if __name__ == "__main__":   
+    dir_start = pathlib.Path.cwd().joinpath('test')
+    dir_zip = pathlib.Path.cwd().joinpath('zip')
     main()
