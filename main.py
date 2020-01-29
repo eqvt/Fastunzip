a08c0f3ca41f2c146da808ab4168b6ef92631de2
eqvt <eqvtdev@gmail.com>
2020-01-29 09:19:17 +0100

    initial

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ __version__ = "0.1.0"
 
 def main():    
     print(f"Your current directory is: {dir_start}")
+    action = input("Write 'Start' to proceed: ")
+    if action == "Start"S:
+        print("Sucessful")
+    else:
+        print("Quiting...")
+        exit()
 
 if __name__ == "__main__":
     # This is executed from the command line 
